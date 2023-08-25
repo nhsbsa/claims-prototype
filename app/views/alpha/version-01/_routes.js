b47615d59cc75039b3c2d0494abcffe329e180d0
Elaine Russell <elaine.russell4@nhs.net>
2023-08-25 11:12:51 +0100

    Progress update
    Working on screens for UR

---
+++ app/views/alpha/version-01/_routes.js
@@ -0,0 +1,43 @@
+const express = require('express')
+const router = express.Router()
+
+const axios = require('axios');
+
+
+// Claims - Screen per question - With navigation
+// ----------------------------
+
+//What type of claim is it? 
+router.post('/route-with-nav-claim-type', function(req,res){
+    var type = req.session.data['type']
+    if (type == "Main - first half year"){
+        res.redirect('/alpha/version-01/claims/screen-per-question/with-navigation/create/target-date')
+    }
+    else if (type == "Supplemental - second half year"){
+        res.redirect('/alpha/version-01/claims/screen-per-question/with-navigation/create/sub-reference')
+    }
+    else {
+        res.redirect('/alpha/version-01/claims/screen-per-question/with-navigation/create/claim-type')
+    }
+
+})
+
+// Claims - Screen per question - Without navigation
+// ----------------------------
+
+//What type of claim is it? 
+router.post('/route-without-nav-claim-type', function(req,res){
+    var type = req.session.data['type']
+    if (type == "Main - first half year"){
+        res.redirect('/alpha/version-01/claims/screen-per-question/without-navigation/create/target-date')
+    }
+    else if (type == "Supplemental - second half year"){
+        res.redirect('/alpha/version-01/claims/screen-per-question/without-navigation/create/sub-reference')
+    }
+    else {
+        res.redirect('/alpha/version-01/claims/screen-per-question/without-navigation/create/claim-type')
+    }
+
+})
+
+module.exports = router
