0b0fd466f41252cf2c00fb3699d548a9afb631d8
Elaine Russell <elaine.russell4@nhs.net>
2023-10-25 16:14:50 +0100

    Upload invoices journey

--- app/views/alpha/version-03/_routes.js
+++ app/views/alpha/version-03/_routes.js
@@ -34,40 +34,70 @@ router.post('/create-claim-validation', function(req,res){
 router.post('/search-treatments-filter', function(req,res){
     var status = req.session.data['filter-status']
     if (status == "all treatments" ) {
-        res.redirect('/alpha/version-03/claims/treatment-forms/with-treatments')
+        res.redirect('/alpha/version-03/claims/invoices/with-treatments')
         }
     else if (status == "to check"){
-            res.redirect('/alpha/version-03/claims/treatment-forms/to-be-checked')
+            res.redirect('/alpha/version-03/claims/invoices/to-be-checked')
             }
     else if (status == "in progress"){
-        res.redirect('/alpha/version-03/claims/treatment-forms/in-progress')
+        res.redirect('/alpha/version-03/claims/invoices/in-progress')
         }
     else if (status == "accepted" ) {
-        res.redirect('/alpha/version-03/claims/treatment-forms/accepted')
+        res.redirect('/alpha/version-03/claims/invoices/accepted')
         }
     else if (status == "contested"){
-        res.redirect('/alpha/version-03/claims/treatment-forms/contested')
+        res.redirect('/alpha/version-03/claims/invoices/contested')
         }
     else if (status == "withdrawn"){
-            res.redirect('/alpha/version-03/claims/treatment-forms/withdrawn')
+            res.redirect('/alpha/version-03/claims/invoices/withdrawn')
         }
     else {
-        res.redirect('/alpha/version-03/claims/treatment-forms/with-treatments')
+        res.redirect('/alpha/version-03/claims/invoices/with-treatments')
     }
 
 })
 
-//Delete claim
+//Delete claim (post MVP)
 router.post('/route_delete_claim', function(req,res){
     var confirmation = req.session.data['delete-claim']
     if (confirmation == "yes" ) {
-        res.redirect('/alpha/version-03/claims/summary/delete-confirmation')
+        res.redirect('/alpha/version-03/post-mvp/claim-summary/delete-confirmation')
         }
     else if (confirmation == "no"){
-            res.redirect('/alpha/version-03/claims/summary/no-treatments')
+            res.redirect('/alpha/version-03/post-mvp/claim-summary/not-started')
             }
     else {
-        res.redirect('/alpha/version-03/claims/summary/delete-check')
+        res.redirect('/alpha/version-03/post-mvp/claim-summary/delete-check')
+    }
+
+})
+
+//Upload additional treatments
+router.post('/route-additional-upload', function(req,res){
+    var upload = req.session.data['additional-upload']
+    if (upload == "yes" ) {
+        res.redirect('/alpha/version-03/claims/invoices/upload/xml/index')
+        }
+    else if (upload == "no"){
+            res.redirect('/alpha/version-03/claims/invoices/upload/xml/cya')
+            }
+    else {
+        res.redirect('/alpha/version-03/claims/invoices/upload/xml/additional-file')
+    }
+
+})
+
+//Upload additional invoices
+router.post('/route-additional-invoice', function(req,res){
+    var method = req.session.data['additional-invoice']
+    if (upload == "bulk" ) {
+        res.redirect('/alpha/version-03/claims/invoices/upload/additional')
+        }
+    else if (upload == "single"){
+            res.redirect('/alpha/version-03/claims/invoices/create/index')
+            }
+    else {
+        res.redirect('/alpha/version-03/claims/invoices/upload/options')
     }
 
 })
