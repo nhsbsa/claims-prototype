c20a0caed1c113d705a533b86bc6d7abdbaa319b
Elaine Russell <elaine.russell4@nhs.net>
2024-01-12 14:32:06 +0000

    Update routes.js

--- app/routes.js
+++ app/routes.js
@@ -11,4 +11,5 @@ router.use('/alpha/version-01', require('./views/alpha/version-01/_routes'));
 router.use('/alpha/version-02', require('./views/alpha/version-02/_routes'));
 router.use('/alpha/version-03', require('./views/alpha/version-03/_routes'));
 router.use('/alpha/version-04', require('./views/alpha/version-04/_routes'));
+router.use('/alpha/version-05', require('./views/alpha/version-05/_routes'));
 module.exports = router;
