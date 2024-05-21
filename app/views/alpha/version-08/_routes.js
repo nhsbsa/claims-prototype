ec4ddd9229fce0520928d27d0b7b311e0fed2f9e
Elaine Russell <elaine.russell4@nhs.net>
2024-05-21 14:25:57 +0100

    Upload invoice journey
    Changes made to the journey to accommodate the scope change of the removal of EDI.

--- app/views/alpha/version-08/_routes.js
+++ app/views/alpha/version-08/_routes.js
@@ -36,9 +36,6 @@ router.post('/route-upload-method', function(req,res){
     if (method == "RINA" ) {
         res.redirect('/alpha/version-08/claims/rina/actual-cost/6784-france/invoices/upload/xml/index')
         }
-    else if (method == "EDI"){
-            res.redirect('/alpha/version-08/claims/rina/actual-cost/6784-france/invoices/upload/xml/index')
-            }
     else {
         res.redirect('/alpha/version-08/claims/rina/actual-cost/6784-france/invoices/upload/index')
     }
@@ -56,11 +53,11 @@ router.post('/route-cancel-upload-invoices', function(req,res){
     var back = req.session.data['referer'] || "/";
 
     if (cancel == "yes" ) {
-        res.redirect('/alpha/version-08/claims/summary/not-started')
+        res.redirect('/alpha/version-08/claims/rina/actual-cost/6784-france/summary/not-started')
     } else if (cancel == "no"){
         return res.redirect(back)
     } else {
-        res.redirect('/alpha/version-08/claims/invoices/upload/cancel-confirmation')
+        res.redirect('/alpha/version-08/claims/rina/actual-cost/6784-france/invoices/upload/cancel-confirmation')
     }
 
 })
@@ -75,11 +72,11 @@ router.post('/route-delete-invoice-file', function(req,res){
     var back = req.session.data['referer'] || "/";
 
     if (cancel == "yes" ) {
-        res.redirect('/alpha/version-08/claims/invoices/upload/xml/index')
+        res.redirect('/alpha/version-08/claims/rina/actual-cost/6784-france/invoices/upload/xml/index')
     } else if (cancel == "no"){
         return res.redirect(back)
     } else {
-        res.redirect('/alpha/version-08/claims/invoices/upload/delete-confirmation')
+        res.redirect('/alpha/version-08/claims/rina/actual-cost/6784-france/invoices/upload/xml/delete-confirmation')
     }
 
 })
