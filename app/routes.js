8c624e5ce392c7b394d3d8a2f1c4c77c6e47fe57
Elaine Russell <elaine.russell4@nhs.net>
2024-06-03 11:47:38 +0100

    Initial publish of version 9

--- app/routes.js
+++ app/routes.js
@@ -18,6 +18,7 @@ router.use('/alpha/version-05', require('./views/alpha/version-05/_routes'));
 router.use('/alpha/version-06', require('./views/alpha/version-06/_routes'));
 router.use('/alpha/version-07', require('./views/alpha/version-07/_routes'));
 router.use('/alpha/version-08', require('./views/alpha/version-08/_routes'));
+router.use('/alpha/version-09', require('./views/alpha/version-09/_routes'));
 
 // clear session data - link in footer
 router.post('/clear-session-data/', (req, res) => {
